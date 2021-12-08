16cc662fd73a9d97eb452119174b8ee574f9eba6
ALagodzinska <anastasijalagodzinska@gmail.com>
2021-12-08 20:55:34 +0200

    Comment by Anastasija

--- AttendanceApp.BLL/Program.cs
+++ AttendanceApp.BLL/Program.cs
@@ -7,7 +7,7 @@ namespace AttendanceApp.BLL
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            //comment
+            //hello 
         }
     }
 }
